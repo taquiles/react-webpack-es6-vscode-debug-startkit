f4303466dc35e0ba0b8920dfc7db98e40c793d79
Pedro Galvão <taquiles@gmail.com>
2017-03-22 23:30:19 +0000

    Update devServer.js

--- devServer.js
+++ devServer.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config.dev');
 
-const port = process.env.npm_package_config_port || 3001;
+const port = process.env.npm_package_config_port || 3000;
 const host = process.env.npm_package_config_host || 'localhost';
 
 new WebpackDevServer(webpack(config), {
